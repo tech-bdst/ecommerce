04eba1ef5f04ec12b3d2265eb021b99dcda8215c
tech-bdst <60012193+tech-bdst@users.noreply.github.com>
2020-04-16 21:36:46 -0400

    Proyecto React y Files del HomePage

---
+++ src/HomePage.compnent.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item"> 
+      <div className='content'>
+          <h1 className='title'>Titulo</h1>
+          <span className='subtitle'>klk</span>
+      </div>
+      </div>
+      <div className="menu-item"> 
+      <div className='content'>
+          <h1 className='title'>Titulo</h1>
+          <span className='subtitle'>klk</span>
+      </div>
+      </div>
+      <div className="menu-item"> 
+      <div className='content'>
+          <h1 className='title'>Titulo</h1>
+          <span className='subtitle'>klk</span>
+      </div>
+      </div>
+      <div className="menu-item"> 
+      <div className='content'>
+          <h1 className='title'>Titulo</h1>
+          <span className='subtitle'>klk</span>
+      </div>
+      </div>
+      <div className="menu-item"> 
+      <div className='content'>
+          <h1 className='title'>Titulo</h1>
+          <span className='subtitle'>klk</span>
+      </div>
+      </div>
+    </div>
+  </div>
+)
+export default HomePage;
